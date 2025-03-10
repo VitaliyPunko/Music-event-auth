3604e58021d0c906175614c2480dbae1d809f504
vitaliy_punko <sapoghnik23@gmail.com>
2025-03-10 21:13:27 +0300

    create test controller

--- src/main/java/vpunko/musiceventauth/controller/TelegramAuthController.java
+++ src/main/java/vpunko/musiceventauth/controller/TelegramAuthController.java
@@ -2,9 +2,11 @@ package vpunko.musiceventauth.controller;
 
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import vpunko.musiceventauth.service.JwtTokenService;
-import vpunko.musiceventauth.service.TelegramAuthValidator;
 import vpunko.musiceventauth.token.TelegramAuthenticationToken;
 
 import java.util.HashMap;
@@ -44,15 +46,4 @@ public class TelegramAuthController {
         }
     }
 
-//    @GetMapping("/token/telegram")
-//    public Map<String, String> getAccessToken(@RequestParam Map<String, String> params) {
-//        Authentication authentication = authenticationManager.authenticate(new TelegramAuthenticationToken(params));
-//
-//        if (authentication.isAuthenticated()) {
-//            String accessToken = jwtTokenService.generateToken(params);
-//            return Map.of("access_token", accessToken, "token_type", "Bearer");
-//        } else {
-//            return Map.of("message", "Authentication failed");
-//        }
-//    }
 }

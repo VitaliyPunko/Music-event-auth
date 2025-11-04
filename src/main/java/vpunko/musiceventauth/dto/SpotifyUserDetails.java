189dede01549da56adf3cb66093bfd7de02cfb6e
vitaliy_punko <sapoghnik23@gmail.com>
2025-11-04 20:52:32 +0300

    comment all database settings to test app in k8s

--- src/main/java/vpunko/musiceventauth/dto/SpotifyUserDetails.java
+++ src/main/java/vpunko/musiceventauth/dto/SpotifyUserDetails.java
@@ -1,32 +1,32 @@
-package vpunko.musiceventauth.dto;
-
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import vpunko.musiceventauth.entity.User;
-
-import java.util.Collection;
-import java.util.List;
-
-public class SpotifyUserDetails implements UserDetails {
-
-    private final User user;
-
-    public SpotifyUserDetails(User user) {
-        this.user = user;
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
-    }
-
-    @Override
-    public String getPassword() {
-        return user.getPassword();
-    }
-
-    @Override
-    public String getUsername() {
-        return user.getEmail();
-    }
-}
+//package vpunko.musiceventauth.dto;
+//
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import vpunko.musiceventauth.entity.User;
+//
+//import java.util.Collection;
+//import java.util.List;
+//
+//public class SpotifyUserDetails implements UserDetails {
+//
+//    private final User user;
+//
+//    public SpotifyUserDetails(User user) {
+//        this.user = user;
+//    }
+//
+//    @Override
+//    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        return List.of();
+//    }
+//
+//    @Override
+//    public String getPassword() {
+//        return user.getPassword();
+//    }
+//
+//    @Override
+//    public String getUsername() {
+//        return user.getEmail();
+//    }
+//}
